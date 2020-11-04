7c72bba06c18ad1e263836b20151f5829207049d
ArtemSlipchencko <68220186+ArtemSlipchencko@users.noreply.github.com>
2020-11-04 17:12:03 +0200

    scrollTo

---
+++ src/js/renderImage.js
@@ -0,0 +1,32 @@
+const list = document.querySelector('.gallery');
+
+export const renderImage = function(images) {
+
+    [...images.hits].forEach(el => {
+
+        list.insertAdjacentHTML('beforeend',
+        `<li class="photo-card">
+            <img height="400px" width="300px" src="${el.webformatURL}" alt="" />
+            <div class="stats">
+                <p class="stats-item">
+                    <i class="material-icons">thumb_up</i>
+                    ${el.likes}
+                </p>
+                <p class="stats-item">
+                    <i class="material-icons">visibility</i>
+                    ${el.views}
+                </p>
+                <p class="stats-item">
+                    <i class="material-icons">comment</i>
+                    ${el.comments}
+                </p>
+                <p class="stats-item">
+                    <i class="material-icons">cloud_download</i>
+                    ${el.downloads}
+                </p>
+            </div>
+        </li>`);
+
+    });
+
+};
--- src/js/searchImage.js
+++ src/js/searchImage.js
@@ -1,19 +1,15 @@
+import {renderImage} from './renderImage';
+
 export const input = document.querySelector('[name="query"]');
+
 const list = document.querySelector('.gallery');
 let page = 1;
 let keyWord = '';
 let renderTimes = 1;
 
-const loadMore = function() {
+const getUrl = function(word, page) {
 
-    const screenY = document.documentElement.scrollHeight;
-    searchImage();
-    console.log(screenY);
-    window.scrollTo({
-        top: screenY,
-        behavior: 'smooth'
-    });
-    
+    return `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${word}&page=${page}&per_page=12&key=18951897-f7110a11ebc58b866f93acf70`;
 
 };
 
@@ -25,66 +21,39 @@ export const searchImage = function() {
 
         page += 1;
 
-        fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${word}&page=${page}&per_page=12&key=18951897-f7110a11ebc58b866f93acf70`)
+        fetch(getUrl(word, page))
         .then(data => data.json())
-        .then(data => renderImage(data));
+        .then(data => {
 
-        
+            const screenY = document.documentElement.scrollHeight;
+            renderImage(data);
+
+            window.scrollTo({
+                top: screenY - 50,
+                behavior: 'smooth'
+            });
+
+        });
 
     } else {
 
         list.innerHTML = '';
 
-        fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${word}&page=${page}&per_page=12&key=18951897-f7110a11ebc58b866f93acf70`)
+        fetch(getUrl(word, page))
         .then(data => data.json())
         .then(data => renderImage(data));
-        
-        const screenY = document.documentElement.scrollHeight;
+
+    };
         
         if (renderTimes === 1) {
 
             document.body.insertAdjacentHTML('beforeend', `<button type="button" class="load-more">Load more...</button>`);
-            document.querySelector('.load-more').addEventListener('click', loadMore);
+            document.querySelector('.load-more').addEventListener('click', searchImage);
             renderTimes += 1;
 
-        }
-        
+        };
         
         keyWord = word;
 
-    };
-
-    
-
 };
 
-const renderImage = function(images) {
-
-    [...images.hits].forEach(el => {
-
-        list.innerHTML += 
-        `<li class="photo-card">
-            <img height="400px" width="300px" src="${el.webformatURL}" alt="" />
-            <div class="stats">
-                <p class="stats-item">
-                    <i class="material-icons">thumb_up</i>
-                    ${el.likes}
-                </p>
-                <p class="stats-item">
-                    <i class="material-icons">visibility</i>
-                    ${el.views}
-                </p>
-                <p class="stats-item">
-                    <i class="material-icons">comment</i>
-                    ${el.comments}
-                </p>
-                <p class="stats-item">
-                    <i class="material-icons">cloud_download</i>
-                    ${el.downloads}
-                </p>
-            </div>
-        </li>`;
-
-    });
-
-};
